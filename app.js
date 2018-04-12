7195020f3847cb4233138ea188add96dee21017d
cliffgor <clierozigy@gmail.com>
2018-04-12 16:24:00 +0300

    Added a food peramiters

--- app.js
+++ app.js
@@ -20,3 +20,42 @@ server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
 });
+
+var inMemoryStorage = new builder.MemoryBotStorage();
+
+// This is a dinner reservation bot that uses a waterfall technique to prompt users for input.
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        session.send("Welcome to the deployment.");
+        builder.Prompts.time(session, "Please provide a reservation date and time (e.g.: June 6th at 5pm)");
+    },
+    function (session, results) {
+        session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
+        builder.Prompts.text(session, "How many people are in your party?");
+    },
+
+    function (session, results) {
+        session.dialogData.food = results.response;
+        builder.Prompts.text(session, "What would you like to eat?");
+    },
+
+    function (session, results) {
+        session.dialogData.partySize = results.response;
+        builder.Prompts.text(session, "Whose name will this reservation be under?");
+    },
+
+
+   
+
+    function (session, results) {
+        session.dialogData.reservationName = results.response;
+
+        // Process request and display reservation details
+        session.send(`Reservation confirmed. <br> Reservation details: 
+        <br/>Reservation name: ${session.dialogData.reservationName} 
+        <br/>Date/Time: ${session.dialogData.reservationDate}
+        <br/>Party size: ${session.dialogData.partySize} 
+        <br/>Food: ${session.dialogData.food}`);
+        session.endDialog();
+    }
+]).set('storage', inMemoryStorage); // Register in-memory storage
