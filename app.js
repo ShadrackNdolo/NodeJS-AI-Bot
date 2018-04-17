303dede05e3909e32715247c6c3839091c20a6a4
ShadrackNdolo <shadrack.winters@outlook.com>
2018-04-17 17:45:50 +0300

    Debug OK, Starting to add questions

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat connector for communicating with the Bot Framework Service
@@ -17,7 +17,7 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
+var bot = new builder.UniversalBot(connector, function(session) {
     session.send("You said: %s", session.message.text);
 });
 
@@ -25,29 +25,29 @@ var inMemoryStorage = new builder.MemoryBotStorage();
 
 // This is a dinner reservation bot that uses a waterfall technique to prompt users for input.
 var bot = new builder.UniversalBot(connector, [
-    function (session) {
+    function(session) {
         session.send("Welcome to the deployment.");
         builder.Prompts.time(session, "Please provide a reservation date and time (e.g.: June 6th at 5pm)");
     },
-    function (session, results) {
-        session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
-        builder.Prompts.text(session, "How many people are in your party?");
-    },
-
-    function (session, results) {
-        session.dialogData.food = results.response;
+    function(session, results) {
+        session.dialogData.food = builder.EntityRecognizer.resolveTime([results.response]);
         builder.Prompts.text(session, "What would you like to eat?");
     },
 
-    function (session, results) {
+    function(session, results) {
         session.dialogData.partySize = results.response;
-        builder.Prompts.text(session, "Whose name will this reservation be under?");
+        builder.Prompts.text(session, "How many people are in your party?");
+    },
+
+    function(session, results) {
+        session.dialogData.reservationName = results.response;
+        builder.Prompts.text(session, "What name would this booking be under?");
     },
 
 
 
 
-    function (session, results) {
+    function(session, results) {
         session.dialogData.reservationName = results.response;
 
         // Process request and display reservation details
